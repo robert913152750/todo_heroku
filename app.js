fec8860411b69ecb9816ff9bd38a02a43d75a4c8
Robert <robert913152750@gmail.com>
2020-04-14 19:08:45 +0800

    Add connect-flash for UI messages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const passport = require("passport");
+const flash = require("connect-flash");
 const port = 3000;
 
 //判別開發環境
@@ -33,6 +34,9 @@ app.use(
   })
 );
 
+//use flash
+app.use(flash());
+
 //use passport
 app.use(passport.initialize());
 app.use(passport.session());
@@ -44,6 +48,8 @@ require("./config/passport")(passport);
 app.use((req, res, next) => {
   res.locals.user = req.user;
   res.locals.isAuthenticated = req.isAuthenticated(); // 辨識使用者是否已經登入的變數，讓 view 可以使用
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
   next();
 });
 
